3524c1b098a229e155f073e7d99d69702e48f7d6
BAISALI D <bdas32691@gmail.com>
2024-06-26 11:19:41 +0530

    first commit

--- app2/urls.py
+++ app2/urls.py
@@ -5,4 +5,6 @@ file_name="anything"
 urlpatterns = [
     path('admin/',admin,name='admin'),
     path('admin2/',admin2,name='admin2'),
+    path('jinja_print/',jinja_print,name='jinja_print'),
+    path('jinja_operation/',jinja_operation,name='jinja_operation'),
 ]
--- app2/views.py
+++ app2/views.py
@@ -3,5 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 def admin(response):
     return HttpResponse('<h1> hello every one this is from app2 Goodly</h1>')
-def admin2(response):
-    return render(response,'index.html')
+def admin2(request):
+    return render(request,'index.html')
+def jinja_print(request):
+    d={'name':'Baishu','age':24}
+    return render(request,'jinja_print.html',context=d)
+def jinja_operation(request):
+    d={'name':'XYZ','age':26}
+    return render(request,'jinja_operation.html',context=d)
